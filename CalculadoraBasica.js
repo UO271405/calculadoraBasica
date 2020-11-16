a1889e5e0f84465f8e82dca25c26ff679f7e5617
Luis <UO271405@uniovi.es>
2020-11-16 20:08:09 +0100

    ficheros de la practica3_3 de sew

---
+++ CalculadoraBasica.js
@@ -0,0 +1,83 @@
+"use strict";
+class CalculadoraBasica {
+    
+    constructor (){
+        this.pantalla = "";
+        this.valorEnMemoria = "";
+        this.resuelto = false;
+    }
+    digitos(digito){
+        if(this.resuelto == true && digito != 0){
+            this.pantalla = "";
+            document.getElementById("pantalla").value = "";
+            this.resuelto = false;
+        }
+        this.pantalla += digito;
+        document.getElementById("pantalla").value = this.pantalla;
+    }
+    punto(){
+        this.pantalla += ".";
+        document.getElementById("pantalla").value = this.pantalla;
+    }
+    suma(){
+        if(this.resuelto == true)   //por si se quiere seguir haciendo operaciones con el resultado
+            this.resuelto = false;
+        this.pantalla += "+";
+        document.getElementById("pantalla").value = this.pantalla;
+    }
+    resta(){
+        if(this.resuelto == true)   //por si se quiere seguir haciendo operaciones con el resultado
+            this.resuelto = false;
+        this.pantalla += "-";
+        document.getElementById("pantalla").value = this.pantalla;
+    }
+    multiplicacion(){
+        if(this.resuelto == true)   //por si se quiere seguir haciendo operaciones con el resultado
+            this.resuelto = false;
+        this.pantalla += "*";
+        document.getElementById("pantalla").value = this.pantalla;
+    }
+    division(){
+        if(this.resuelto == true)   //por si se quiere seguir haciendo operaciones con el resultado
+            this.resuelto = false;
+        this.pantalla += "/";
+        document.getElementById("pantalla").value = this.pantalla;
+    }
+    mrc(){
+        if(this.pantalla == "" || !isNaN(document.getElementById("pantalla").value)){ //si no hay nada en la pantalla o es un numero sin ningun signo al lado
+            this.pantalla = this.valorEnMemoria;    //la memoria sera el nuevo numero a computar
+            document.getElementById("pantalla").value = this.pantalla;
+        }else{  //si quieres usar ese valor en memoria para la operación que ya está en pantalla
+            this.pantalla += this.valorEnMemoria;
+            document.getElementById("pantalla").value = this.pantalla;
+        }
+    }
+    mMenos(){
+        if(!isNaN(document.getElementById("pantalla").value)){  //si es un numero
+            this.valorEnMemoria -= document.getElementById("pantalla").value;   //restarselo a la memoria
+        }
+    }
+    mMas(){
+        if(!isNaN(document.getElementById("pantalla").value)){  //si es un numero
+            this.valorEnMemoria += document.getElementById("pantalla").value;   //sumarselo a la memoria
+        }
+    }
+    borrar(){
+        this.pantalla = "";
+        this.valorEnMemoria = "";
+        document.getElementById("pantalla").value = this.pantalla;
+    }
+    igual(){
+        try {
+            var result = eval(this.pantalla);
+            if(typeof result == "number"){
+                this.pantalla = result;
+                document.getElementById("pantalla").value = this.pantalla;
+                this.resuelto = true;
+            }
+        } catch (error) {   //reset
+            this.borrar();
+        }
+    }
+}
+var calculadora = new CalculadoraBasica();
